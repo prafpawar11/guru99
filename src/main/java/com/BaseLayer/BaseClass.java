7d34d2abf3b95596b481dec3e189e76547de5d1a
prafpawar11 <74486812+prafpawar11@users.noreply.github.com>
2023-09-22 20:04:54 +0530

    Update BaseClass.java

--- src/main/java/com/BaseLayer/BaseClass.java
+++ src/main/java/com/BaseLayer/BaseClass.java
@@ -6,11 +6,8 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BaseClass {
 
@@ -33,22 +30,10 @@ public class BaseClass {
 	public static void iniliazation(String browsername) {
 
 		if (browsername.equals("chrome")) {
-			WebDriverManager.chromedriver().setup();
+			
 
 			driver = new ChromeDriver();
-		} else if (browsername.equals("edge")) {
-			System.setProperty("webdriver.edge.driver",
-					System.getProperty("user.dir") + "//AllDrivers//msedgedriver.exe");
-
-			driver = new EdgeDriver();
-		} else if (browsername.equals("opera")) {
-			WebDriverManager.operadriver().setup();
-			driver = new OperaDriver();
-		}
-		else if(browsername.equals("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver =new FirefoxDriver();
-		}
+		} 
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
